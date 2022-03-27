123b728411487974b8fdda8b4ee9790824a6e7c8
kristjanb <kristjan.bleiweis@gmail.com>
2022-03-27 19:37:41 +0200

    dodal sem datoteke, ki smo jih obravnavali na prvih dveh predavanjih

--- Zapiski_naloge_ipd/04 - Tabele/00_00_1D_tabele.cpp
+++ Zapiski_naloge_ipd/04 - Tabele/00_00_1D_tabele.cpp
@@ -13,11 +13,13 @@ using namespace std;
 int main() {
     
 	int tab[10];
+	
+	int prastevila[5] = {2, 3, 5, 7, 11};
+	
 
 	for(int i = 0; i < 10; i++){
 		tab[i] = i * 7;
 	}
-	
 	
 	cout << tab[7] << endl;
 
